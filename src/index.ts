78ed8c85f324d1e10c7f79c871777464e6d86680
forrestwaters <forrestwaters@gmail.com>
2021-11-01 23:41:41 +0800

    build fixes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-import TestComponent from "./TestComponent/TestComponent";
+import Button from "Button/Button";
+import ContentTransitionWrapper from "ContentTransitionWrapper/ContentTransitionWrapper";
+import PillButton from "PillButton/PillButton";
+import PillsGroup from "PillsGroup/PillsGroup";
+import TabContent from "TabContent/TabContent";
+import TabNavigation from "TabNavigation/TabNavigation";
+import Tab from "Tab/Tab";
 
-export { TestComponent };
+export {
+  Button,
+  ContentTransitionWrapper,
+  PillButton,
+  PillsGroup,
+  TabNavigation,
+  TabContent,
+  Tab,
+};
