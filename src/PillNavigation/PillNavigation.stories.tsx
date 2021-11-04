70904bc0a85a1fc1592bf851227d99fdb4af0624
forrestwaters <forrestwaters@gmail.com>
2021-11-05 01:41:31 +0800

    refactors pill navigation

--- src/PillNavigation/PillNavigation.stories.tsx
+++ src/PillNavigation/PillNavigation.stories.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import TabPanel from '../TabPanel/TabPanel';
 import PillNavigationComponent from './PillNavigation';
 
 export default {
   title: 'Component Library/Organisms/Pill Navigation',
   component: PillNavigationComponent,
 };
+const POSITIVE_PANEL = () => (
+  <div>
+    <h1>Positive Items</h1>
+    <p>
+      First page of content: Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit. Sed euismod ipsum velit, eu aliquam eros tincidunt eget. Donec
+      euismod, nisi euismod, nunc nisl aliquet, nunc nisl aliquet, nunc nisl
+      aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc
+      nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet,
+      nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl
+      aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet,
+    </p>
+  </div>
+);
+const NEGATIVE_PANEL = () => (
+  <div>
+    <h1>Negative Items</h1>
+    <p>
+      Second page of content: Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit. Sed euismod ipsum velit, eu aliquam eros tincidunt eget. Donec
+      euismod, nisi euismod, nunc nisl aliquet, nunc nisl aliquet, nunc nisl
+      aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc
+      nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet,
+      nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl
+      aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet,
+    </p>
+  </div>
+);
 
-export const PillNavigation = () => <PillNavigationComponent />;
+export const PillNavigation = () => (
+  <PillNavigationComponent
+    pills={[
+      { text: 'Postive', count: 11 },
+      { text: 'Negative', count: 15 },
+    ]}
+  >
+    <TabPanel>
+      <POSITIVE_PANEL />
+    </TabPanel>
+    <TabPanel>
+      <NEGATIVE_PANEL />
+    </TabPanel>
+  </PillNavigationComponent>
+);
--- src/PillNavigation/PillNavigation.tsx
+++ src/PillNavigation/PillNavigation.tsx
@@ -1,62 +1,43 @@
 // Generated with util/create-component.js
 import React from 'react';
-import TabPanel from '../TabPanel/TabPanel';
+import { TabPanels } from '../TabNavigation/TabNavigation';
 import Pill from '../Pill/Pill';
 
-export default function PillNavigation() {
+export default function PillNavigation({ pills, children }) {
   const [activeTabIndex, setActiveTabIndex] = React.useState(0);
   return (
     <div>
-      <Pills>
-        <Pill
-          active={activeTabIndex === 0}
-          text="Positive"
-          value="pill1"
-          count={11}
-          onClick={() => setActiveTabIndex(0)}
-        />
-        <Pill
-          active={activeTabIndex === 1}
-          text="Negative"
-          value="pill2"
-          count={22}
-          onClick={() => setActiveTabIndex(1)}
-        />
-      </Pills>
-      <div>
-        <TabPanel active={activeTabIndex === 0}>
-          <div>
-            <h1>Positive Items</h1>
-            <p>
-              First page of content: Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Sed euismod ipsum velit, eu aliquam eros
-              tincidunt eget. Donec euismod, nisi euismod, nunc nisl aliquet,
-              nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl
-              aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet,
-              nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl
-              aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet,
-              nunc nisl aliquet, nunc nisl aliquet,
-            </p>
-          </div>
-        </TabPanel>
-        <TabPanel active={activeTabIndex === 1}>
-          <div>
-            <h1>Negative Items</h1>
-            <p>
-              Second page of content: Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Sed euismod ipsum velit, eu aliquam eros
-              tincidunt eget. Donec euismod, nisi euismod, nunc nisl aliquet,
-              nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl
-              aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet,
-              nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl
-              aliquet, nunc nisl aliquet, nunc nisl aliquet, nunc nisl aliquet,
-              nunc nisl aliquet, nunc nisl aliquet,
-            </p>
-          </div>
-        </TabPanel>
-      </div>
+      <Pills
+        pills={pills}
+        activeTabIndex={activeTabIndex}
+        setActiveTabIndex={setActiveTabIndex}
+      />
+      <TabPanels>
+        {children.map((child, index) => {
+          const props = {
+            active: activeTabIndex === index,
+          };
+          if (React.isValidElement(child)) {
+            return React.cloneElement(child, props);
+          } else {
+            return child;
+          }
+        })}
+      </TabPanels>
     </div>
   );
 }
 
-const Pills = ({ children }) => <div style={{ display: 'flex' }}>{children}</div>;
+const Pills = ({ pills, activeTabIndex, setActiveTabIndex }) => (
+  <div style={{ display: 'flex' }}>
+    {pills.map((pill, index) => (
+      <Pill
+        active={activeTabIndex === index}
+        text={pill.text}
+        value={pill.value}
+        count={pill.count}
+        onClick={() => setActiveTabIndex(index)}
+      />
+    ))}
+  </div>
+);
